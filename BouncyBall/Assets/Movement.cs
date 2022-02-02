d295698fbb35d564f8c9118699054e6b745fed0c
SpartanCrabAM88 <samikmathur@sfhs.com>
2022-02-02 15:39:57 -0500

    Fixed movement bug
    Vertical movement was broken

--- BouncyBall/Assets/Movement.cs
+++ BouncyBall/Assets/Movement.cs
@@ -8,20 +8,27 @@ public class Movement : MonoBehaviour
     [SerializeField] float force;
     [SerializeField] float jumpForce;
     Vector3 spdLastFrame;
-    Collider col;
+    [SerializeField] float dropForce;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
-        col = GetComponent<Collider>();
     }
 
     private void FixedUpdate()
     {
-        rb.AddForce(new Vector3(Input.GetAxis("Horizontal") * force * Time.deltaTime, 0f, Input.GetAxis("Vertical")) * force * Time.deltaTime);
+        rb.AddForce(new Vector3(Input.GetAxis("Horizontal") * force * Time.deltaTime, 0f, Input.GetAxis("Vertical") * force * Time.deltaTime));
         print("Vel: " + rb.velocity);
         print("Accel: " + (rb.velocity - spdLastFrame) * 1000);
         spdLastFrame = rb.velocity;
+        if (Input.GetButtonDown("Drop"))
+        {
+            Debug.Log("Reached");
+            rb.angularVelocity = Vector3.zero;
+            rb.velocity = Vector3.zero;
+            rb.AddForce(Vector3.down * dropForce * Time.deltaTime);
+        }
+
     }
 
     private void OnCollisionStay(Collision collision)
