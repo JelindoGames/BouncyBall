1a2638f727f5de2a5ae58aaeb0197e58596fae54
SpartanCrabAM88 <samikmathur@sfhs.com>
2022-02-08 10:07:39 -0500

    Revert "Fixed movement bug"
    This reverts commit d295698fbb35d564f8c9118699054e6b745fed0c.

--- BouncyBall/Assets/Movement.cs
+++ BouncyBall/Assets/Movement.cs
@@ -8,27 +8,20 @@ public class Movement : MonoBehaviour
     [SerializeField] float force;
     [SerializeField] float jumpForce;
     Vector3 spdLastFrame;
-    [SerializeField] float dropForce;
+    Collider col;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        col = GetComponent<Collider>();
     }
 
     private void FixedUpdate()
     {
-        rb.AddForce(new Vector3(Input.GetAxis("Horizontal") * force * Time.deltaTime, 0f, Input.GetAxis("Vertical") * force * Time.deltaTime));
+        rb.AddForce(new Vector3(Input.GetAxis("Horizontal") * force * Time.deltaTime, 0f, Input.GetAxis("Vertical")) * force * Time.deltaTime);
         print("Vel: " + rb.velocity);
         print("Accel: " + (rb.velocity - spdLastFrame) * 1000);
         spdLastFrame = rb.velocity;
-        if (Input.GetButtonDown("Drop"))
-        {
-            Debug.Log("Reached");
-            rb.angularVelocity = Vector3.zero;
-            rb.velocity = Vector3.zero;
-            rb.AddForce(Vector3.down * dropForce * Time.deltaTime);
-        }
-
     }
 
     private void OnCollisionStay(Collision collision)
