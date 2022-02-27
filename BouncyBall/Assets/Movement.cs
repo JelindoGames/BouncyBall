7b5ef9a4e9d656f3c0b98b200ccfe4da65397639
JelindoGames <51799620+JelindoGames@users.noreply.github.com>
2022-02-27 18:09:27 -0500

    Cool Controls

--- BouncyBall/Assets/Movement.cs
+++ BouncyBall/Assets/Movement.cs
@@ -7,24 +7,92 @@ public class Movement : MonoBehaviour
     Rigidbody rb;
     [SerializeField] float force;
     [SerializeField] float jumpForce;
+    [SerializeField] float jumpForceOnBounce;
     [SerializeField] float dropForce;
+    [SerializeField] float origSpeedCap;
+    [SerializeField] float fakeFrictionAmt;
+    [SerializeField] float fakeFrictionAmtAir;
     Vector3 spdLastFrame;
     Collider col;
     public PhysicMaterial bouncey;
     public PhysicMaterial defaultMat;
     [SerializeField] bool grounded;
+    [SerializeField] bool groundedForPeriod;
+    [SerializeField] int framesNeededGroundedForPeriod;
     [SerializeField] bool braking;
     public SphereCollider breakCol;
+
+    // For Perfect Bounce
+    [SerializeField] float perfectBounceMaxWait;
+    [SerializeField] float perfectBounceInputBuffer;
+    public bool inPerfectBounceWindow;
+    bool canPressPerfectBounce = true;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
         col = GetComponent<SphereCollider>();
+        Physics.autoSimulation = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && canPressPerfectBounce)
+        {
+            StartCoroutine("PerfectBounceTimer");
+        }
+
+        if (!grounded)
+        {
+            groundedForPeriod = false;
+        }
+        if (grounded && !groundedForPeriod)
+        {
+            StartCoroutine("WaitForGroundedTwoFrames");
+        }
+    }
+
+    IEnumerator PerfectBounceTimer()
+    {
+        float timer = 0;
+        inPerfectBounceWindow = true;
+        canPressPerfectBounce = false;
+
+        // Perfect bounce window
+        while (timer < perfectBounceMaxWait)
+        {
+            timer += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+
+        inPerfectBounceWindow = false;
+
+        // Wait for it to be okay to press perfect bounce again
+        while (timer < perfectBounceInputBuffer)
+        {
+            timer += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+
+        canPressPerfectBounce = true;
+    }
+
+    IEnumerator WaitForGroundedTwoFrames()
+    {
+        int frames = 1;
+
+        while (frames < framesNeededGroundedForPeriod)
+        {
+            frames++;
+            yield return new WaitForEndOfFrame();
+        }
+        groundedForPeriod = grounded;
     }
 
     private void FixedUpdate()
     {
         breakCol.radius = (2.642857143E-4f * Breakable.KineticEnergy(rb)) + 0.5704761905f;
+
         if (Input.GetAxis("Jump") > 0.005)
         {
             gameObject.GetComponent<Collider>().material = bouncey;
@@ -43,16 +111,32 @@ public class Movement : MonoBehaviour
         }
         else
         {
-            rb.AddForce(new Vector3(Input.GetAxis("Horizontal") * force * Time.deltaTime, 0f, Input.GetAxis("Vertical") * force * Time.deltaTime));
+            Vector2 velFwd = new Vector2(rb.velocity.x, rb.velocity.z);
+            rb.AddForce(new Vector3(Input.GetAxis("Horizontal") * force * Time.fixedDeltaTime, 0f, Input.GetAxis("Vertical") * force * Time.fixedDeltaTime));
+            if (velFwd.magnitude > origSpeedCap)
+            {
+                print("velFwd.magnitude: " + velFwd.magnitude);
+                float frictionAmt = grounded ? fakeFrictionAmt : fakeFrictionAmtAir;
+                Vector3 newVelXZ = rb.velocity.normalized * (rb.velocity.magnitude - (frictionAmt * Time.fixedDeltaTime));
+                rb.velocity = new Vector3(newVelXZ.x, rb.velocity.y, newVelXZ.z);
+            }
         }
 
-        if (Input.GetAxis("Jump") > 0.005 && grounded)
+        if (grounded && inPerfectBounceWindow)
         {
-            rb.AddForce(new Vector3(0f, Input.GetAxis("Jump") * jumpForce * Time.deltaTime, 0f));
+            if (groundedForPeriod)
+            {
+                rb.AddForce(new Vector3(0f, Input.GetAxis("Jump") * jumpForce * Time.deltaTime, 0f));
+            }
+            else
+            {
+                rb.AddForce(new Vector3(0f, Input.GetAxis("Jump") * jumpForceOnBounce * Time.deltaTime, 0f));
+            }
         }
         print("Vel: " + rb.velocity);
         print("Accel: " + (rb.velocity - spdLastFrame) * 1000);
         spdLastFrame = rb.velocity;
+        Physics.Simulate(Time.fixedDeltaTime);
     }
 
     private void OnCollisionExit(Collision collision)
