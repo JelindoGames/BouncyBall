6c4c35956201338a3776cd6259b48a21dec24dbf
rishavsarma5 <rishavsarma5@gmail.com>
2022-03-31 19:05:39 -0400

    cleaned up some scripts

--- BouncyBall/Assets/Scripts/SpeedBoost.cs
+++ BouncyBall/Assets/Scripts/SpeedBoost.cs
@@ -7,17 +7,4 @@ public class SpeedBoost : MonoBehaviour
     public float addedForce = 5;
     public Vector3 directionSpecifier = Vector3.zero;
     public bool needsBounce;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
