31c67fc465d3e823a2346b8f4a4249408f11b66a
SpartanCrabAM88 <samikmathur@sfhs.com>
2022-04-15 17:47:16 -0400

    Boss Level
    HUNGRY!!!1

--- BouncyBall/Assets/Scripts/BlockBoss.cs
+++ BouncyBall/Assets/Scripts/BlockBoss.cs
@@ -13,8 +13,6 @@ public class BlockBoss : MonoBehaviour
     public List<GameObject> blocks;
     public GameObject middleBlock;
     public GameObject shot;
-    public Collider top;
-    public Collider body;
 
     public float middleBlockHeight;
     public float topHeight;
@@ -63,11 +61,6 @@ public class BlockBoss : MonoBehaviour
 
     private IEnumerator Origin()
     {
-        body.enabled = true;
-        top.enabled = true;
-        body.GetComponent<Renderer>().enabled = false;
-        top.GetComponent<Renderer>().enabled = false;
-
         foreach (GameObject g in blocks)
         {
             g.transform.position = new Vector3(g.transform.position.x, currentHeight, g.transform.position.z);
@@ -75,19 +68,15 @@ public class BlockBoss : MonoBehaviour
         middleBlock.transform.position = new Vector3(middleBlock.transform.position.x, currentHeight, middleBlock.transform.position.z);
         yield return new WaitForSeconds(0.2f);
         if (health % 2 == 1)
-            phase = Phase.BLOCKS;
-        else
             phase = Phase.SHOT;
+        else
+            phase = Phase.BLOCKS;
         yield return null;
     }
 
 
     private IEnumerator Blocks()
     {
-        body.enabled = false;
-        top.enabled = true;
-        body.GetComponent<Renderer>().enabled = true;
-        top.GetComponent<Renderer>().enabled = false;
 
         transform.position = new Vector3(transform.position.x, middleBlockHeight + 50, transform.position.z);
         List<GameObject> temp = new List<GameObject>();
@@ -130,16 +119,11 @@ public class BlockBoss : MonoBehaviour
 
     private IEnumerator Shot()
     {
-        body.enabled = true;
-        top.enabled = false;
-        body.GetComponent<Renderer>().enabled = false;
-        top.GetComponent<Renderer>().enabled = true;
-
         int shots = 15;
         for (int i = 0; i < shots; i++)
         {
             transform.Rotate(new Vector3(0f, 360f / shots, 0f));
-            GameObject g = Instantiate(shot, transform.position + (transform.forward * 5) + (transform.up * 10), new Quaternion());
+            GameObject g = Instantiate(shot, transform.position + (transform.forward * 5) + (transform.up * 10.5f), new Quaternion());
             g.transform.Rotate(0, (360f / shots) * i, 0f);
             yield return new WaitForSeconds(0.2f);
         }
@@ -147,19 +131,37 @@ public class BlockBoss : MonoBehaviour
         yield return null;
     }
 
-    public void Hit()
+    public void CallHit()
+    {
+        StartCoroutine("Hit");
+    }
+
+    public IEnumerator Hit()
     {
         health--;
         if(health <= 0)
         {
             Destroy(gameObject);
         }
-        player.position = playerTeleportPoint.position;
         player.GetComponent<Rigidbody>().velocity = Vector3.zero;
         player.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+        yield return StartCoroutine("MoveToOrigin");
         //player.GetComponent<Movement>().DropParticles(false);
 
         Reset();
+        yield return null;
+    }
+
+    private IEnumerator MoveToOrigin()
+    {
+        while (Vector3.Distance(playerTeleportPoint.position, player.position) >= 0.5f)
+        {
+            player.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            player.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            player.position = Vector3.Lerp(player.position, playerTeleportPoint.position, 0.1f);
+            yield return new WaitForEndOfFrame();
+        }
+        yield return null;
     }
 
     private void Reset()
